74307bc2daf8f34fc63de634af19d6bd4093178b
Lip-7 <zhighofficial@gmail.com>
2023-06-29 17:07:01 +0200

    created config files for doctors table

---
+++ config/doctors.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    [
+        "nome" => "Mario",
+        "cognome" => "Rossi",
+        "numero_cellulare" => "+39 3331234567",
+        "url_immagine_profilo" => "https://example.com/mario_rossi.jpg",
+        "servizi" => "Visita Dermatologica: 200€; Esportazione Verruche: 150€",
+    ],
+    [
+        "nome" => "Laura",
+        "cognome" => "Bianchi",
+        "numero_cellulare" => "+39 3349876543",
+        "url_immagine_profilo" => "https://example.com/laura_bianchi.jpg",
+        "servizi" => "Elettrocardiogramma: 80€; Holter pressorio: 120€",
+    ],
+    [
+        "nome" => "Giuseppe",
+        "cognome" => "Verdi",
+        "numero_cellulare" => "+39 3398765432",
+        "url_immagine_profilo" => "https://example.com/giuseppe_verdi.jpg",
+        "servizi" => "Visita Odontoiatrica: 100€; Sbiancamento Dentale: 150€",
+    ],
+    [
+        "nome" => "Francesca",
+        "cognome" => "Russo",
+        "numero_cellulare" => "+39 3387654321",
+        "url_immagine_profilo" => "https://example.com/francesca_russo.jpg",
+        "servizi" => "Visita Ginecologica: 150€; Ecografia Ostetrica: 200€",
+    ],
+    [
+        "nome" => "Marco",
+        "cognome" => "Conte",
+        "numero_cellulare" => "+39 3376543210",
+        "url_immagine_profilo" => "https://example.com/marco_conte.jpg",
+        "servizi" => "Visita Neurologica: 180€; Elettromiografia: 250€",
+    ],
+];
