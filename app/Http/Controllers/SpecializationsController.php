d034f102ac4c2596194634a33bfc19216b03031d
raffaelecapaldo <raffaelecpl@gmail.com>
2023-06-29 15:33:05 +0200

    Added all file for the migrations of the DB schema

---
+++ app/Http/Controllers/SpecializationsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Specializations;
+use App\Http\Requests\StoreSpecializationsRequest;
+use App\Http\Requests\UpdateSpecializationsRequest;
+
+class SpecializationsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSpecializationsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Specializations $specializations)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Specializations $specializations)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSpecializationsRequest $request, Specializations $specializations)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Specializations $specializations)
+    {
+        //
+    }
+}
