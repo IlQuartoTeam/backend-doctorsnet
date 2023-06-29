e9c6bafe3c79880761efeb53aa34ad7f690f7387
raffaelecapaldo <raffaelecpl@gmail.com>
2023-06-29 17:06:19 +0200

    Added User seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            ["name" => 'Paolo',
+            'surname' => 'Rossi',
+            'email' => 'paolorossi@gmail.com',
+            'password' => 'PaoloRossi2023' ],
+            ["name" => 'Giovanni',
+            'surname' => 'Palumbo',
+            'email' => 'giovannipalumbo@gmail.com',
+            'password' => 'GiovanniPalumbo' ],
+            ["name" => 'Francesco',
+            'surname' => 'Sassi',
+            'email' => 'francosassi@gmail.com',
+            'password' => 'FrancoSassi2023' ],
+            ["name" => 'Maria',
+            'surname' => 'Rossa',
+            'email' => 'mariarossa@gmail.com',
+            'password' => 'PaolinaRossina2023' ],
+            ["name" => 'Franco',
+            'surname' => 'Ciccio',
+            'email' => 'franchino@gmail.com',
+            'password' => 'franchino2029' ],
+        ];
+
+        foreach ($users as $user) {
+            $newUser = new User();
+            $newUser->name = $user['name'];
+            $newUser->surname = $user['surname'];
+            $newUser->email = $user['email'];
+            $newUser->password = $user['password'];
+            $newUser->slug = $newUser['name'] . '-' . $newUser['surname'] . '-' . rand(1,300);
+        }
+    }
+}
