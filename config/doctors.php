74643ef11b2732b83205621ec134110511eb6269
filecc <fil.gia@live.it>
2023-06-30 19:00:38 +0200

    modified seeder doctors to include lat and lon

--- config/doctors.php
+++ config/doctors.php
@@ -6,34 +6,44 @@ return [
         "profile_image_url" => "https://picsum.photos/500/500",
         "examinations" => "Visita Dermatologica: 200€; Esportazione Verruche: 150€",
         "address" => "Via del Quirinale, 18, 00184",
-        "city" => "Roma"
+        "city" => "Roma",
+        "lat" => "41.8994808",
+        "long" => "12.4872242"
     ],
     [
         "phone" => "393349876543",
         "profile_image_url" => "https://picsum.photos/500/500",
         "examinations" => "Elettrocardiogramma: 80€; Holter pressorio: 120€",
         "address" => "Via Nazionale, 48, 00184",
-        "city" => "Roma"
+        "city" => "Roma",
+        "lat" => "41.8999422",
+        "long" => "12.492242"
     ],
     [
         "phone" => "393398765432",
         "profile_image_url" => "https://picsum.photos/500/500",
         "examinations" => "Visita Odontoiatrica: 100€; Sbiancamento Dentale: 150€",
         "address" => "Piazza di Spagna, 20, 00187",
-        "city" => "Roma"
+        "city" => "Roma",
+        "lat" => "41.9061951",
+        "long" => "12.4822574"
     ],
     [
         "phone" => "393387654321",
         "profile_image_url" => "https://picsum.photos/500/500",
         "examinations" => "Visita Ginecologica: 150€; Ecografia Ostetrica: 200€",
         "address" => "Via Appia Nuova, 123, 00179",
-        "city" => "Roma"
+        "city" => "Roma",
+        "lat" => "41.8700706",
+        "long" => "12.5286991"
     ],
     [
         "phone" => "393376543210",
         "profile_image_url" => "https://picsum.photos/500/500",
         "examinations" => "Visita Neurologica: 180€; Elettromiografia: 250€",
         "address" => "Via dei Fori Imperiali, 1, 00186",
-        "city" => "Roma"
+        "city" => "Roma",
+        "lat" => "41.8927016",
+        "long" => "12.4879575"
     ],
 ];
