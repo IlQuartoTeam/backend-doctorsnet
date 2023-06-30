44bde84864707a6e64570ade4ae6535934f32fcf
raffaelecapaldo <raffaelecpl@gmail.com>
2023-06-30 11:33:17 +0200

    Added index of Specialization

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-
+use App\Http\Controllers\SpecializationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +24,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/user', [AuthController::class, 'user'])->middleware('auth:sanctum');
+
+Route::resource('specializations', SpecializationController::class)->only(['index']);
