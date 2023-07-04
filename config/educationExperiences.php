d9e30462518c84ff86ccf629e31f855a1622f120
filecc <fil.gia@live.it>
2023-07-04 18:21:45 +0200

    file experience

---
+++ config/educationExperiences.php
@@ -0,0 +1,144 @@
+<?php
+return [
+    [
+        "doctor_id" => 1,
+        "name" => "Medicina e Chirurgia - Università di Bologna",
+        "type" => "education",
+        "start_date" => "1990-09-01",
+        "end_date" => "1996-07-15"
+    ],
+    [
+        "doctor_id" => 2,
+        "name" => "Medicina e Chirurgia - Università di Firenze",
+        "type" => "education",
+        "start_date" => "1991-09-01",
+        "end_date" => "1997-07-15"
+    ],
+    [
+        "doctor_id" => 3,
+        "name" => "Medicina e Chirurgia - Università di Milano",
+        "type" => "education",
+        "start_date" => "1992-09-01",
+        "end_date" => "1998-07-15"
+    ],
+    [
+        "doctor_id" => 4,
+        "name" => "Medicina e Chirurgia - Università di Napoli Federico II",
+        "type" => "education",
+        "start_date" => "1993-09-01",
+        "end_date" => "1999-07-15"
+    ],
+    [
+        "doctor_id" => 5,
+        "name" => "Medicina e Chirurgia - Università di Padova",
+        "type" => "education",
+        "start_date" => "1994-09-01",
+        "end_date" => "2000-07-15"
+    ],
+    [
+        "doctor_id" => 6,
+        "name" => "Medicina e Chirurgia - Università di Pisa",
+        "type" => "education",
+        "start_date" => "1995-09-01",
+        "end_date" => "2001-07-15"
+    ],
+    [
+        "doctor_id" => 7,
+        "name" => "Medicina e Chirurgia - Università di Roma La Sapienza",
+        "type" => "education",
+        "start_date" => "1996-09-01",
+        "end_date" => "2002-07-15"
+    ],
+    [
+        "doctor_id" => 8,
+        "name" => "Medicina e Chirurgia - Università di Torino",
+        "type" => "education",
+        "start_date" => "1997-09-01",
+        "end_date" => "2003-07-15"
+    ],
+    [
+        "doctor_id" => 9,
+        "name" => "Medicina e Chirurgia - Università di Genova",
+        "type" => "education",
+        "start_date" => "1998-09-01",
+        "end_date" => "2004-07-15"
+    ],
+    [
+        "doctor_id" => 10,
+        "name" => "Medicina e Chirurgia - Università di Bari",
+        "type" => "education",
+        "start_date" => "1999-09-01",
+        "end_date" => "2005-07-15"
+    ],
+    [
+        "doctor_id" => 11,
+        "name" => "Medicina e Chirurgia - Università di Bologna",
+        "type" => "education",
+        "start_date" => "1990-09-01",
+        "end_date" => "1996-07-15"
+    ],
+    [
+        "doctor_id" => 12,
+        "name" => "Medicina e Chirurgia - Università di Firenze",
+        "type" => "education",
+        "start_date" => "1991-09-01",
+        "end_date" => "1997-07-15"
+    ],
+    [
+        "doctor_id" => 13,
+        "name" => "Medicina e Chirurgia - Università di Milano",
+        "type" => "education",
+        "start_date" => "1992-09-01",
+        "end_date" => "1998-07-15"
+    ],
+    [
+        "doctor_id" => 14,
+        "name" => "Medicina e Chirurgia - Università di Napoli Federico II",
+        "type" => "education",
+        "start_date" => "1993-09-01",
+        "end_date" => "1999-07-15"
+    ],
+    [
+        "doctor_id" => 15,
+        "name" => "Medicina e Chirurgia - Università di Padova",
+        "type" => "education",
+        "start_date" => "1994-09-01",
+        "end_date" => "2000-07-15"
+    ],
+    [
+        "doctor_id" => 16,
+        "name" => "Medicina e Chirurgia - Università di Milano",
+        "type" => "education",
+        "start_date" => "1992-09-01",
+        "end_date" => "1998-07-15"
+    ],
+    [
+        "doctor_id" => 17,
+        "name" => "Medicina e Chirurgia - Università di Roma La Sapienza",
+        "type" => "education",
+        "start_date" => "1995-09-01",
+        "end_date" => "2001-07-15"
+    ],
+    [
+        "doctor_id" => 18,
+        "name" => "Medicina e Chirurgia - Università di Torino",
+        "type" => "education",
+        "start_date" => "1996-09-01",
+        "end_date" => "2002-07-15"
+    ],
+    [
+        "doctor_id" => 19,
+        "name" => "Medicina e Chirurgia - Università di Genova",
+        "type" => "education",
+        "start_date" => "1997-09-01",
+        "end_date" => "2003-07-15"
+    ],
+    [
+        "doctor_id" => 20,
+        "name" => "Medicina e Chirurgia - Università di Palermo",
+        "type" => "education",
+        "start_date" => "1998-09-01",
+        "end_date" => "2004-07-15"
+    ]
+
+    ];
