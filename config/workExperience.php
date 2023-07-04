71307baa51dbd4520b13d28dd10be7ba3b8bafc6
filecc <fil.gia@live.it>
2023-07-04 18:31:25 +0200

    seeeder experience

--- config/workExperience.php
+++ config/workExperience.php
@@ -1,140 +1,131 @@
 <?php
 return [
     [
-        "doctor_id" => 1,
         "name" => "Medico - Ospedale San Raffaele di Milano",
         "type" => "work",
         "start_date" => "2000-01-01",
         "end_date" => "2010-01-01"
     ],
     [
-        "doctor_id" => 2,
         "name" => "Medico - Ospedale Niguarda di Milano",
         "type" => "work",
         "start_date" => "2001-02-01",
         "end_date" => "2011-02-01"
     ],
     [
-        "doctor_id" => 3,
         "name" => "Medico - Ospedale San Carlo di Milano",
         "type" => "work",
         "start_date" => "2002-03-01",
         "end_date" => ""
     ],
     [
-        "doctor_id" => 4,
         "name" => "Medico - Ospedale San Paolo di Milano",
         "type" => "work",
         "start_date" => "2003-04-01",
         "end_date" => ""
     ],
     [
-        "doctor_id" => 5,
         "name" => "Medico - Ospedale San Paolo di Milano",
         "type" => "work",
         "start_date" => "2003-04-01",
         "end_date" => ""
     ],
     [
-        "doctor_id" => 6,
         "name" => "Medico - Ospedale Fatebenefratelli di Milano",
         "type" => "work",
         "start_date" => "2004-05-01",
         "end_date" => ""
     ],
     [
-        "doctor_id" => 7,
         "name" => "Medico - Ospedale Maggiore di Milano",
         "type" => "work",
         "start_date" => "2005-06-01",
         "end_date" => ""
     ],
     [
-        "doctor_id" => 8,
         "name" => "Medico - Ospedale Luigi Sacco di Milano",
         "type" => "work",
         "start_date" => "2006-07-01",
         "end_date" => ""
     ],
     [
-        "doctor_id" => 9,
         "name" => "Medico - Ospedale San Gerardo di Monza",
         "type" => "work",
         "start_date" => "2007-08-01",
         "end_date" => ""
     ],
     [
-        "doctor_id" => 10,
+
         "name" => "Medico di reparto",
         "type" => "work",
         "start_date" => "2002-07-15",
         "end_date" => "2012-07-15"
     ],
     [
-        "doctor_id" => 11,
+
         "name" => "Primario di Pronto Soccorso",
         "type" => "work",
         "start_date" => "2005-02-28",
         "end_date" => "2015-02-28"
     ],
     [
-        "doctor_id" => 12,
+
         "name" => "Medico di pronto soccorso",
         "type" => "work",
         "start_date" => "2010-09-10",
         "end_date" => "2020-09-10"
     ],
     [
-        "doctor_id" => 13,
+
         "name" => "Medico Generico - Primario",
         "type" => "work",
         "start_date" => "2008-11-20",
         "end_date" => "2018-11-20"
     ],
     [
-        "doctor_id" => 14,
+
         "name" => "GinMedico di pronto soccorsoecologo",
         "type" => "work",
         "start_date" => "2004-06-03",
         "end_date" => "2014-06-03"
     ],
     [
-        "doctor_id" => 15,
+
         "name" => "Medico di reparto",
         "type" => "work",
         "start_date" => "2012-04-17",
         "end_date" => "2022-04-17"
     ],
     [
-        "doctor_id" => 16,
+
         "name" => "Medico di reparto",
         "type" => "work",
         "start_date" => "2006-09-28",
         "end_date" => "2016-09-28"
     ],
     [
-        "doctor_id" => 17,
+
         "name" => "Medico di reparto",
         "type" => "work",
         "start_date" => "2003-12-10",
         "end_date" => "2013-12-10"
     ],
     [
-        "doctor_id" => 18,
+
         "name" => "Medico di reparto",
         "type" => "work",
         "start_date" => "2007-08-05",
         "end_date" => "2017-08-05"
     ],
     [
-        "doctor_id" => 19,
+
         "name" => "Medico di reparto",
         "type" => "work",
         "start_date" => "2014-03-19",
         "end_date" => "2024-03-19"
     ],
     [
-        "doctor_id" => 20,
+
         "name" => "Medico di reparto",
         "type" => "work",
         "start_date" => "2001-10-31",
--- database/seeders/ExperiencesSeeder.php
+++ database/seeders/ExperiencesSeeder.php
@@ -13,38 +13,26 @@ class ExperiencesSeeder extends Seeder
      */
     public function run(): void
     {
-        $degrees = [
-            'Laurea in Medicina e Chirurgia - Università di Napoli',
-            'Laurea in Medicina e Chirurgia - Università di Milano',
-            'Laurea in Medicina e Chirurgia - Università di Roma',
-            'Laurea in Medicina e Chirurgia - Università di Genova',
-            'Laurea in Medicina e Chirurgia - Università di Trieste',
-            'Laurea in Medicina e Chirurgia - Università di Torino',
-        ];
-        $works = [
-            'Primario - Ospedale Fatebene Fratelli',
-            'Medico Sala Operatoria - Ospedale Gemelli Milano',
-            'Primario - Ospedale Cannizzaro di Catania',
-            'Medico di Reparto - Ospedale CTO di Torino',
-            'Medico di Reparto - Ospedale Oftalmico di Torino'
-        ];
+        $degrees = config("educationExperiences");
+        $works = config("workExperience");
+        $numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100];
 
-        foreach ([1, 2, 3, 4, 5] as $doctor_id) {
-            $yearStartEducation = rand(1975, 1995);
+
+        foreach ($numbers as $doctor_id) {
+            $randNumber = rand(0, count($degrees) -1);
             DB::table('experiences')->insert([
                 'doctor_id' => $doctor_id,
-                'name' => $degrees[rand(0, count($degrees) -1)],
+                'name' => $degrees[$randNumber]->name,
                 'type' => 'education',
-                'start_date' => $yearStartEducation.'-'.rand(1,12).'-'.rand(1,27),
-                'end_date' => ($yearStartEducation+6).'-'.rand(1,12).'-'.rand(1,27)
+                'start_date' => $degrees[$randNumber]->start_date,
+                'end_date' => $degrees[$randNumber]->end_date
             ]);
-            $yearStartWork = rand(1996, 2023);
             DB::table('experiences')->insert([
                 'doctor_id' => $doctor_id,
-                'name' => $works[rand(0, count($works) -1)],
+                'name' => $works[$randNumber]->name,
                 'type' => 'work',
-                'start_date' => $yearStartWork.'-'.rand(1,12).'-'.rand(1,27),
-                'end_date' => null
+                'start_date' => $works[$randNumber]->start_date,
+                'end_date' => $works[$randNumber]->end_date
             ]);
         }
     }
