6722ef0e5964f85c9c1c5b388ef88024697a06bd
Lip-7 <zhighofficial@gmail.com>
2023-07-04 18:04:34 +0200

    added users in usersList

--- config/usersList.php
+++ config/usersList.php
@@ -9,10 +9,10 @@ return [
         'password' => 'PaoloRossi2023'
     ], //maschio
     [
-        "name" => 'Giovanni',
+        "name" => 'Giovanna',
         'surname' => 'Palumbo',
-        'email' => 'giovannipalumbo@gmail.com',
-        'password' => 'GiovanniPalumbo'
+        'email' => 'giovannapalumbo@gmail.com',
+        'password' => 'GiovannaPalumbo'
     ], //maschio
     [
         "name" => 'Francesco',
@@ -152,5 +152,470 @@ return [
         'email' => 'elisabianchi@gmail.com',
         'password' => 'doctorsnet'
     ],
+    [
+        "name" => 'Chiara',
+        'surname' => 'Verdi',
+        'email' => 'chiaraverdi@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Alessio',
+        'surname' => 'Galli',
+        'email' => 'alessiogalli@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Sara',
+        'surname' => 'Rossi',
+        'email' => 'sararossi@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Davide',
+        'surname' => 'Bianchi',
+        'email' => 'davidebianchi@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Federico',
+        'surname' => 'Verdi',
+        'email' => 'federicoverdi@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Lorenzo',
+        'surname' => 'Galli',
+        'email' => 'lorenzogalli@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Roberto',
+        'surname' => 'Rossi',
+        'email' => 'robertorossi@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Giulia',
+        'surname' => 'Bianchi',
+        'email' => 'giuliabianchi@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Marco',
+        'surname' => 'Galli',
+        'email' => 'marcogalli@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Elisa',
+        'surname' => 'Rossi',
+        'email' => 'elisarossi@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Chiara',
+        'surname' => 'Bianchi',
+        'email' => 'chiarabianchi@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Alessio',
+        'surname' => 'Verdi',
+        'email' => 'alessioverdi@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Sara',
+        'surname' => 'Fumagalli',
+        'email' => 'sarafumagalli@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Davide',
+        'surname' => 'Rossi',
+        'email' => 'daviderossi@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Federico',
+        'surname' => 'Bianchi',
+        'email' => 'federicobianchi@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Lorenzo',
+        'surname' => 'Verdi',
+        'email' => 'lorenzoverdi@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Roberto',
+        'surname' => 'Galli',
+        'email' => 'robertogalli@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Giulia',
+        'surname' => 'Rossi',
+        'email' => 'giuliarossi@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Simone',
+        'surname' => 'Bianchi',
+        'email' => 'simonebianchi@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Marco',
+        'surname' => 'Verdi',
+        'email' => 'marcoverdi@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Elisa',
+        'surname' => 'Galli',
+        'email' => 'elisagalli@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Chiara',
+        'surname' => 'Rossi',
+        'email' => 'chiararossi@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Alessio',
+        'surname' => 'Bianchi',
+        'email' => 'alessiobianchi@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Sara',
+        'surname' => 'Verdi',
+        'email' => 'saraverdi@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Davide',
+        'surname' => 'Galli',
+        'email' => 'davidegalli@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Federico',
+        'surname' => 'Rossi',
+        'email' => 'federicorossi@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    /* Da qui in poi non ha alternato */
+    [
+        "name" => 'Lorenzo',
+        'surname' => 'Bianchi',
+        'email' => 'lorenzobianchi@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Giulia',
+        'surname' => 'Galli',
+        'email' => 'giuliagalli@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Simone',
+        'surname' => 'Rossi',
+        'email' => 'simonerossi@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+
+    [
+        "name" => 'Chiara',
+        'surname' => 'Galli',
+        'email' => 'chiaragalli@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Alessio',
+        'surname' => 'Rossi',
+        'email' => 'alessiorossi@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Sara',
+        'surname' => 'Bianchi',
+        'email' => 'sarabianchi@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Davide',
+        'surname' => 'Verdi',
+        'email' => 'davideverdi@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Sara',
+        'surname' => 'Capaldo',
+        'email' => 'saracapaldo@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Federico',
+        'surname' => 'Galli',
+        'email' => 'federicogalli@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Maria',
+        'surname' => 'Verrone',
+        'email' => 'mariaverrone@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Lorenzo',
+        'surname' => 'Rossi',
+        'email' => 'lorenzorossi@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Jessica',
+        'surname' => 'Zibra',
+        'email' => 'jessicazibra@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Roberto',
+        'surname' => 'Bianchi',
+        'email' => 'robertobianchi@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Roberta',
+        'surname' => 'Giampapa',
+        'email' => 'robertagiampapa@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Simone',
+        'surname' => 'Galli',
+        'email' => 'simonegalli@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Greta',
+        'surname' => 'Spampinato',
+        'email' => 'gretaspampinato@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Marco',
+        'surname' => 'Rossi',
+        'email' => 'marcorossi@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Clelia',
+        'surname' => 'Fradella',
+        'email' => 'cleliafradella@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Roberto',
+        'surname' => 'Verdi',
+        'email' => 'robertoverdi@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Giulia',
+        'surname' => 'Ferrari',
+        'email' => 'giuliaferrari@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Simone',
+        'surname' => 'Russo',
+        'email' => 'simonerusso@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Marco',
+        'surname' => 'Martini',
+        'email' => 'marcomartini@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Elisa',
+        'surname' => 'Conti',
+        'email' => 'elisaconti@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Chiara',
+        'surname' => 'Romano',
+        'email' => 'chiararomano@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Alessio',
+        'surname' => 'Gatti',
+        'email' => 'alessiogatti@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Sara',
+        'surname' => 'Bianco',
+        'email' => 'sarabianco@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Davide',
+        'surname' => 'Marchetti',
+        'email' => 'davidemarchetti@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Federico',
+        'surname' => 'Fontana',
+        'email' => 'federicofontana@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Lorenzo',
+        'surname' => 'Mancini',
+        'email' => 'lorenzomancini@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Roberto',
+        'surname' => 'Rossini',
+        'email' => 'robertorossini@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Giulia',
+        'surname' => 'Santoro',
+        'email' => 'giuliasantoro@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Simone',
+        'surname' => 'Ferri',
+        'email' => 'simoneferri@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Marco',
+        'surname' => 'Ricci',
+        'email' => 'marcoricci@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Elisa',
+        'surname' => 'Moretti',
+        'email' => 'elisamoretti@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Chiara',
+        'surname' => 'Lombardi',
+        'email' => 'chiaralombardi@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Alessio',
+        'surname' => 'Colombo',
+        'email' => 'alessiocolombo@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Sara',
+        'surname' => 'Galli',
+        'email' => 'saragalli@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Davide',
+        'surname' => 'Rizzo',
+        'email' => 'daviderizzo@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Federico',
+        'surname' => 'Marini',
+        'email' => 'federicomarini@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Lorenzo',
+        'surname' => 'De Luca',
+        'email' => 'lorenzodeluca@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Roberto',
+        'surname' => 'Gentile',
+        'email' => 'robertogentile@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Giulia',
+        'surname' => 'Costa',
+        'email' => 'giuliacosta@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Simone',
+        'surname' => 'Fabbri',
+        'email' => 'simonefabbri@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Marco',
+        'surname' => 'Caruso',
+        'email' => 'marcocaruso@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Elisa',
+        'surname' => 'Barbieri',
+        'email' => 'elisabarbieri@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Chiara',
+        'surname' => 'Ferrara',
+        'email' => 'chiaraferrara@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Alessio',
+        'surname' => 'Serra',
+        'email' => 'alessioserra@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Sara',
+        'surname' => 'Cattaneo',
+        'email' => 'saracattaneo@gmail.com',
+        'password' => 'doctorsnet'
+    ],
+    [
+        "name" => 'Davide',
+        'surname' => 'Villa',
+        'email' => 'davidevilla@gmail.com',
+        'password' => 'doctorsnet'
+    ],
 
 ];
+
+/* $checkArray = [];
+$doubles = [];
+foreach ($users as $key => $value) {
+    if (!in_array($value, $checkArray)) {
+        array_push($checkArray, $value);
+    } else {
+        array_push($doubles, $value);
+    }
+}
+
+var_dump(count($users));
+var_dump($doubles); */
